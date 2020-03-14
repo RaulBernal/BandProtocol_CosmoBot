7f7a040809ee3a7e321c272debd3ac440c9a83d7
Raul Bernal <bernal.raul@gmail.com>
2020-03-14 17:19:33 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+#CONFIG THE BOT / CHAIN 
+path_to_daemon = '/home/administrador/go/bin/gaiad'  #  Put the path to the exec
+path_to_cli = '/home/administrador/go/bin/gaiacli '  # Path to cli 
+url_api = 'http://localhost:36657/'   # Url to API
+cosmos_address='cosmos1ft3qq5hragz807n55ajq8qa7ymvvszzujg09q8' 
+
+#About Telegram API with Botogram
+token = '594662433:AAExBRaUl3O050MoQU5qwe2q2kCuXJ_8hyX2ow' #  Put your own token
