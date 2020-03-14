990f9d23a1388f03b22176af674740179b506e4a
Raul Bernal <bernal.raul@gmail.com>
2020-03-14 17:35:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 #CONFIG THE BOT / CHAIN: example for a Regen Testnet
-path_to_daemon = '/home/administrador/go/bin/rgnxd'  #  Put the path to the exec
-path_to_cli = '/home/administrador/go/bin/rgnxdcli '  # Path to cli 
+path_to_daemon = '/home/administrador/go/bin/xrnd'  #  Put the path to the exec
+path_to_cli = '/home/administrador/go/bin/xrncli '  # Path to cli 
 url_api = 'http://localhost:26657/'   # Url to API
 cosmos_address='xrn:1mxnuednhzvh99aadgrgde53ss7vzslv477rzth' 
 chain_id='kontraua'
-
 
 #About Telegram API with Botogram - create with BotFather bot
 token = '594662433:AAExBRaUl3O050MoQU5qwe2q2kCuXJ_8hyX2ow' #  Put your own token
