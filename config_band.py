73a66327c129a0c459b893c7df0bafbb2c3782b6
Raul Bernal <bernal.raul@gmail.com>
2020-08-09 16:28:32 +0200

    Create config_band.py

---
+++ config_band.py
@@ -0,0 +1,11 @@
+#CONFIG THE BOT / CHAIN: 
+path_to_daemon = '/usr/local/bin/bandd'  #  Put the path to the daemon
+path_to_cli = '/usr/local/bin/bandcli'  # Path to cli 
+path_to_yoda = '/usr/local/bin/yoda'   # Path to Yoda bin
+url_api = 'http://localhost:6657/'   # Url to API
+band_address='band1lt6pj70vrvfd5dxxx95hrv5e8y5zesf00e2a9q'   #Band autodelegate address
+bandvaloper_address='bandvaloper1lt6pj70vrvfd5dxxx95hrv5e8y5zesf0r0w7qt' #Band Validator address
+chain_id='band-guanyu-testnet2'
+
+#About Telegram API with Botogram - create with BotFather bot
+token = '594662433:AAExBRajajaja0MoQU5qwe2q2kCuXJ_8hyX2ow' #  Put your own token
