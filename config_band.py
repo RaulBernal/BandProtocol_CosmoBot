7c840d0ddcb7cc7821363b1ab5cfd682b8ef3f96
Raul Bernal <bernal.raul@gmail.com>
2020-08-15 16:25:20 +0200

    Update config_band.py

--- config_band.py
+++ config_band.py
@@ -6,6 +6,9 @@ url_api = 'http://localhost:6657/'   # Url to API
 band_address='band1lt6pj70vrvfd5dxxx95hrv5e8y5zesf00e2a9q'   #Band autodelegate address
 bandvaloper_address='bandvaloper1lt6pj70vrvfd5dxxx95hrv5e8y5zesf0r0w7qt' #Band Validator address
 chain_id='band-guanyu-testnet2'
+priv_key='wallet-privkey'
+wallet_name='wallet'
+url_explorer='https://guanyu-testnet2.cosmoscan.io'
 
 #About Telegram API with Botogram - create with BotFather bot
 token = '594662433:AAExBRajajaja0MoQU5qwe2q2kCuXJ_8hyX2ow' #  Put your own token
